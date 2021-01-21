db6c3aeea905e11e63966800218120df53b4ddc3
Merve Özçifçi <merveozcifci7@gmail.com>
2021-01-21 16:59:47 +0300

    floatlabel updated

--- src/app/demo/view/floatlabeldemo.component.ts
+++ src/app/demo/view/floatlabeldemo.component.ts
@@ -4,7 +4,13 @@ import {BreadcrumbService} from '../../app.breadcrumb.service';
 
 @Component({
     templateUrl: './floatlabeldemo.component.html',
-    styleUrls: ['./floatlabeldemo.scss']
+    styleUrls: ['./floatlabeldemo.scss'],
+    styles: [`
+        :host ::ng-deep .p-cascadeselect .country-item img.flag {
+            width: 18px;
+            margin-right: .5rem;
+        }
+    `]
 })
 export class FloatLabelDemoComponent implements OnInit {
 
@@ -34,6 +40,14 @@ export class FloatLabelDemoComponent implements OnInit {
 
     value10: any;
 
+    value11: any;
+
+    value12: any;
+
+    countriesCascade: any[];
+
+    selectedCascade: any;
+
     constructor(private countryService: CountryService, private breadcrumbService: BreadcrumbService) {
         this.breadcrumbService.setItems([
             {label: 'Float Label'}
@@ -51,6 +65,83 @@ export class FloatLabelDemoComponent implements OnInit {
         this.countryService.getCountries().then(countries => {
             this.countries = countries;
         });
+
+        this.countriesCascade = [
+            {
+                name: 'Australia',
+                code: 'AU',
+                states: [
+                    {
+                        name: 'New South Wales',
+                        cities: [
+                            {cname: 'Sydney', code: 'A-SY'},
+                            {cname: 'Newcastle', code: 'A-NE'},
+                            {cname: 'Wollongong', code: 'A-WO'}
+                        ]
+                    },
+                    {
+                        name: 'Queensland',
+                        cities: [
+                            {cname: 'Brisbane', code: 'A-BR'},
+                            {cname: 'Townsville', code: 'A-TO'}
+                        ]
+                    },
+
+                ]
+            },
+            {
+                name: 'Canada',
+                code: 'CA',
+                states: [
+                    {
+                        name: 'Quebec',
+                        cities: [
+                            {cname: 'Montreal', code: 'C-MO'},
+                            {cname: 'Quebec City', code: 'C-QU'}
+                        ]
+                    },
+                    {
+                        name: 'Ontario',
+                        cities: [
+                            {cname: 'Ottawa', code: 'C-OT'},
+                            {cname: 'Toronto', code: 'C-TO'}
+                        ]
+                    },
+
+                ]
+            },
+            {
+                name: 'United States',
+                code: 'US',
+                states: [
+                    {
+                        name: 'California',
+                        cities: [
+                            {cname: 'Los Angeles', code: 'US-LA'},
+                            {cname: 'San Diego', code: 'US-SD'},
+                            {cname: 'San Francisco', code: 'US-SF'}
+                        ]
+                    },
+                    {
+                        name: 'Florida',
+                        cities: [
+                            {cname: 'Jacksonville', code: 'US-JA'},
+                            {cname: 'Miami', code: 'US-MI'},
+                            {cname: 'Tampa', code: 'US-TA'},
+                            {cname: 'Orlando', code: 'US-OR'}
+                        ]
+                    },
+                    {
+                        name: 'Texas',
+                        cities: [
+                            {cname: 'Austin', code: 'US-AU'},
+                            {cname: 'Dallas', code: 'US-DA'},
+                            {cname: 'Houston', code: 'US-HO'}
+                        ]
+                    }
+                ]
+            }
+        ];
     }
 
     searchCountry(event) {
