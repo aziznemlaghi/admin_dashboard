bd7d31f10b6f9325080943bcefebd7fd2fb3a3c2
Merve Özçifçi <merveozcifci7@gmail.com>
2021-01-22 16:03:33 +0300

    filter fixed

--- src/app/demo/view/tabledemo.component.ts
+++ src/app/demo/view/tabledemo.component.ts
@@ -50,7 +50,11 @@ export class TableDemoComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.customerService.getCustomersLarge().then(customers => this.customers1 = customers);
+        this.customerService.getCustomersLarge().then(customers => {
+            this.customers1 = customers;
+            // @ts-ignore
+            this.customers1.forEach(customer => customer.date = new Date(customer.date));
+        });
         this.customerService.getCustomersMedium().then(customers => this.customers2 = customers);
         this.customerService.getCustomersMedium().then(customers => this.customers3 = customers);
         this.productService.getProductsWithOrdersSmall().then(data => this.products = data);
